23c6a995aefe4f2ac365502f1ef6113c76a3ed0f
Sankhadip Das <sankhadip10.das@gmail.com>
2023-11-07 23:54:52 +0530

    "second days js code"

--- thirty_day_cahllenge_js/day1.js
+++ thirty_day_cahllenge_js/day1.js
@@ -1,3 +1,12 @@
+const readline = require('readline')
+
+// Create readline interface for input/output
+const rl = readline.createInterface({
+    input : process.stdin,
+    output : process.stdout
+});
+
+
 function reverseNumber(x){
     let reversed  = 0
     while(x!== 0){
@@ -8,7 +17,36 @@ function reverseNumber(x){
     return reversed
 }
 
-const x =32243
-const reversed = reverseNumber(x)
-console.log(reversed)
+// const x =32243
+// const reversed = reverseNumber(x)
+// console.log(reversed)
+// Handle the close event
+rl.on('close', () => {
+    console.log('\nOperation terminated by the user');
+    process.exit(0);
+});
+
+// Catch CTRL+C (SIGINT) and close readline properly
+rl.on('SIGINT', () => {
+    rl.close();
+});
+
+// Catch any unexpected errors
+rl.on('error', (error) => {
+    console.error(`An error occurred: ${error.message}`);
+    rl.close();
+});
+
+
+rl.question("Please enter the number to reverse: ",(input)=>{
+    const num = parseInt(input)
+    if(!isNaN(num)){
+        const reversed = reverseNumber(num)
+        console.log(`The reversed number is: ${reversed}`)
+    }else{
+        console.log("You did't enter a right number")
+    }
+
+    rl.close()
+})
 
---
+++ thirty_day_cahllenge_js/day2.js
@@ -0,0 +1,14 @@
+// ## Challenge 2:
+// Write a JavaScript function that accepts a string as a parameter and converts the first letter of each word into upper case.
+
+// Example string: 'the quick brown fox'
+// Expected Output: 'The Quick Brown Fox '
+function firstLetterCapital(x){
+    for(let i=0;i<x.length;i++){
+        if x[i]! = ' '
+    }
+}
+
+
+let x = 'the quick brown fox'
+value=firstLetterCapital(x)
